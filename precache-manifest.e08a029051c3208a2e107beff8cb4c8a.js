21632141fca71692cbd499f7e9be06783fe875bc
Devon <devonnaidoo13@gmail.com>
2019-05-13 23:47:36 +0200

    Updates

---
+++ precache-manifest.e08a029051c3208a2e107beff8cb4c8a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "e3c5643c324f557eef2ead6a2f54025d",
+    "url": "/rank-finder/static/media/city-aerial-view.e3c5643c.jpg"
+  },
+  {
+    "revision": "1aec54c331643fa04562",
+    "url": "/rank-finder/static/css/main.e8da39b5.chunk.css"
+  },
+  {
+    "revision": "8ac850736ec8bb301a90",
+    "url": "/rank-finder/static/js/runtime~main.3cce9dbb.js"
+  },
+  {
+    "revision": "da9f5124637d3d7243e16105429ff7ae",
+    "url": "/rank-finder/static/media/Simple-Line-Icons.da9f5124.svg"
+  },
+  {
+    "revision": "07c7a5710d93db03f72a",
+    "url": "/rank-finder/static/js/2.5f68705f.chunk.js"
+  },
+  {
+    "revision": "fd60ade892a58c8518c30c49c04caf97",
+    "url": "/rank-finder/static/media/SOUTH-AFRICA-TAXIS.fd60ade8.jpg"
+  },
+  {
+    "revision": "1aec54c331643fa04562",
+    "url": "/rank-finder/static/js/main.9f51ccf5.chunk.js"
+  },
+  {
+    "revision": "0cb0b9c589c0624c9c78dd3d83e946f6",
+    "url": "/rank-finder/static/media/Simple-Line-Icons.0cb0b9c5.woff2"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/rank-finder/static/media/Simple-Line-Icons.d2285965.ttf"
+  },
+  {
+    "revision": "65b3fef3f17d82d430f93f96c7324566",
+    "url": "/rank-finder/static/media/Simple-Line-Icons.65b3fef3.eot"
+  },
+  {
+    "revision": "78f07e2c2a535c26ef21d95e41bd7175",
+    "url": "/rank-finder/static/media/Simple-Line-Icons.78f07e2c.woff"
+  },
+  {
+    "revision": "07c7a5710d93db03f72a",
+    "url": "/rank-finder/static/css/2.f7569c5f.chunk.css"
+  },
+  {
+    "revision": "3b0145e800dbd4d7a73fef1a1c7b312d",
+    "url": "/rank-finder/index.html"
+  }
+];
